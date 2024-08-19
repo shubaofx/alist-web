64142f15cfba543fad4fc31d141f9882c9dabc3b
舒宝 <1052992209@qq.com>
2024-08-19 18:06:18 +0800

    Update ipa.tsx

--- src/pages/home/previews/ipa.tsx
+++ src/pages/home/previews/ipa.tsx
@@ -13,6 +13,7 @@ const Ipa = () => {
   return (
     <FileInfo>
       <HStack spacing="$2">
+        {/* 原生IPA安装 */}
         <Button
           as="a"
           href={
@@ -20,7 +21,7 @@ const Ipa = () => {
             `${api}/i/${safeBtoa(
               encodeURIComponent(objStore.raw_url) +
                 "/" +
-                baseName(encodeURIComponent(objStore.obj.name)),
+                baseName(encodeURIComponent(objStore.obj.name))
             )}.plist`
           }
           onClick={() => {
@@ -29,6 +30,7 @@ const Ipa = () => {
         >
           {t(`home.preview.${installing() ? "installing" : "install"}`)}
         </Button>
+        {/* 巨魔测试 | use install TrollStore IPA*/}
         <Button
           as="a"
           colorScheme="primary"
@@ -42,7 +44,26 @@ const Ipa = () => {
         >
           {t(`home.preview.${trInstalling() ? "tr-installing" : "tr-install"}`)}
         </Button>
+
+        {/* 从这里到
+        下载按钮文件拷贝的位置
+          alist-web\src\pages\home\previews\download.tsx
+         primary  青绿色
+         accent   紫色
+         neutral  灰色
+         success  绿色
+         info     无？
+         warning  橙色
+         danger   红色
+         */}
+        <Button colorScheme="danger" as="a" href={objStore.raw_url} target="_blank">
+          {t("home.preview.download")}
+        </Button>
+        <Button colorScheme="accent" onClick={() => copyCurrentRawLink(true)}>
+          {t("home.toolbar.copy_link")}
+        </Button>
       </HStack>
+      {/* 到这里 还有开头的 <HStack spacing="$2"> 都是自己加的 */}
     </FileInfo>
   )
 }
